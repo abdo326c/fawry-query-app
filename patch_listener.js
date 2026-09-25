ef425f521e51681c82df3ade7118c1980a13d852
abdo326c <50345646+abdo326c@users.noreply.github.com>
2026-09-25 23:15:52 +0300

    chore: remove patch files

--- patch_listener.js
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('app.js', 'utf8');
-const searchStr = "const checkboxes = document.querySelectorAll('#dashboard-bank-filters input[type=\"checkbox\"]');";
-const replaceStr = "document.getElementById('dashboard-group-by')?.addEventListener('change', () => this.loadDashboard());\n        " + searchStr;
-code = code.replace(searchStr, replaceStr);
-fs.writeFileSync('app.js', code);
